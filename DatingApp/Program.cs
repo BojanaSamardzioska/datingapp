2897da12e6991ae25546031aecba6b5d0b26028f
BojanaSamardzioska <b_samardzioska@yahoo.com>
2023-07-06 00:14:21 +0200

    add ssl

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -27,6 +28,7 @@ var app = builder.Build();
 
 //use later
 //app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
