3fc514552af9b3d85d04682d0af19bae3ed6237d
BojanaSamardzioska <b_samardzioska@yahoo.com>
2023-07-08 23:17:13 +0200

    Added middleware exception and controller for testing errors.

--- DatingApp/DTOs/RegisterDto.cs
+++ DatingApp/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace DatingApp.DTOs
         public string Username { get; set; }
 
         [Required]
+        [StringLength(12, MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -1,6 +1,7 @@
 using DatingApp.Data;
 using DatingApp.Extensions;
 using DatingApp.Interfaces;
+using DatingApp.Middleware;
 using DatingApp.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 //if (app.Environment.IsDevelopment())
 //{
 //    app.UseSwagger();
