42d63fa81bfc13caa83678243fa493b7d9620f13
BojanaSamardzioska <b_samardzioska@yahoo.com>
2023-07-12 17:36:49 +0200

    Extended models and account controller for login and register.

--- DatingApp/DTOs/RegisterDto.cs
+++ DatingApp/DTOs/RegisterDto.cs
@@ -8,7 +8,18 @@ namespace DatingApp.DTOs
         public string Username { get; set; }
 
         [Required]
-        [StringLength(12, MinimumLength = 6)]
+        public string KnownAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; } //optional for required to work
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        [StringLength(18, MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
