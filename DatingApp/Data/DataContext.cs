2c5663678b7b8993c408ed228b6e8927d4262c2e
BojanaSamardzioska <b_samardzioska@yahoo.com>
2023-07-14 18:15:23 +0200

    Created Like repo, updated data context and database with new table.

--- DatingApp/Data/DataContext.cs
+++ DatingApp/Data/DataContext.cs
@@ -10,5 +10,28 @@ namespace DatingApp.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+            //one person(source) has liked many users
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //one person(target) has been liked by many users
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
---
+++ DatingApp/Data/LikesRepository.cs
@@ -0,0 +1,63 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Extensions;
+using DatingApp.Helpers;
+using DatingApp.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        private readonly DataContext _context;
+
+        public LikesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+        {
+            return await _context.Likes.FindAsync(sourceUserId, targetUserId);
+        }
+
+        //list of users liked by the currently logged in user or a list of users that liked the logged in user
+        public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
+        {
+            var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
+            var likes = _context.Likes.AsQueryable();
+            
+            //users I liked
+            if(likesParams.Predicate == "liked")
+            {
+                likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
+                users = likes.Select(like => like.TargetUser);
+            }
+
+            //users that like me
+            if(likesParams.Predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.TargetUserId == likesParams.UserId);
+                users = likes.Select(like => like.SourceUser);
+            }
+
+            var likedUsers = users.Select(user => new LikeDto
+            {
+                UserName = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            });
+
+            //instead of ToListAsync
+            return await PagedList<LikeDto>.CreateAync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
+        }
+
+        public async Task<AppUser> GetUserWithLikes(int userId)
+        {
+            return await _context.Users.Include(x => x.LikedUsers).FirstOrDefaultAsync(x => x.Id == userId);
+        }
+    }
+}
