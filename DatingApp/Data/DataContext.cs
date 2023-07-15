f1ba511b0bace56f6548c6ba4bd173e0dc49c44c
BojanaSamardzioska <b_samardzioska@yahoo.com>
2023-07-15 23:35:01 +0200

    Created API for messaging.

--- DatingApp/Data/DataContext.cs
+++ DatingApp/Data/DataContext.cs
@@ -11,6 +11,7 @@ namespace DatingApp.Data
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -32,6 +33,19 @@ namespace DatingApp.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.TargetUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            //messaging
+            //user has received many messages
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //has sent many messages
+            modelBuilder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
